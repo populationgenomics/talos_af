7fe0a87d06c4cfc0d03b2f0b3cfca413095ef24a
MattWellie <mattwellie@gmail.com>
2025-10-17 17:21:23 +1000

    pushing some bits

--- src/talos_af/process_acmg_spec.py
+++ src/talos_af/process_acmg_spec.py
@@ -15,8 +15,14 @@ The other required file is MANE.GRCh38.v1.4.summary.txt.gz from the NCBI FTP sit
 """
 
 import gzip
+import json
+import re
 from argparse import ArgumentParser
 from csv import DictReader
+
+
+P_AND_LP = 'P and LP'
+SPECIFIC_CHANGE = re.compile(r'(p.[A-Z][0-9]+[A-Z]) (?:\w+ )?only')
 
 
 def get_mane_mapping(mane_path: str) -> dict[str, dict[str, str]]:
@@ -36,9 +42,9 @@ def get_mane_mapping(mane_path: str) -> dict[str, dict[str, str]]:
         mane_reader = DictReader(mane_file, delimiter="\t")
         for line in mane_reader:
             symbol = line["symbol"]
-            ensg = line["Ensembl_Gene"]
-            nm_id = line["RefSeq_nuc"]
-            enst = line["Ensembl_nuc"]
+            ensg = line["Ensembl_Gene"].split('.')[0]
+            nm_id = line["RefSeq_nuc"].split('.')[0]
+            enst = line["Ensembl_nuc"].split('.')[0]
             mane_mapping[symbol] = {
                 'ensg': ensg,
                 'nm_id': nm_id,
@@ -48,7 +54,43 @@ def get_mane_mapping(mane_path: str) -> dict[str, dict[str, str]]:
 
 
 def main(input_spec: str, output_path: str, mane: str) -> None:
-    ...
+
+    parsed = {}
+    mane_mapping = get_mane_mapping(mane)
+
+    with open(input_spec, "r") as input_file:
+        reader = DictReader(input_file, delimiter="\t")
+        for row in reader:
+            gene = row['Gene']
+
+            # hard lookup here, we don't expect or allow failures. These are super well known genes
+            mane = mane_mapping[gene]
+
+            row_dict = {
+                'gene': gene,
+                'moi': row['Inheritance'],
+                'gene_id': mane['ensg'],
+                'nm_id': mane['nm_id'],
+                'enst': mane['enst'],
+            }
+
+            vars_to_report = row['Variants to report']
+
+            if 'truncating' in vars_to_report:
+                row_dict['reportable'] = 'consequence'
+                row_dict['specific_type'] = 'truncating'
+
+            elif P_AND_LP in vars_to_report:
+                row_dict['reportable'] = 'all'
+
+            elif result := re.findall(SPECIFIC_CHANGE, vars_to_report):
+                row_dict['reportable'] = 'specific'
+                row_dict['specific_type'] = result[0]
+
+            parsed[mane['ensg']] = row_dict
+
+    with open(output_path, "w", encoding='utf-8') as output_file:
+        json.dump(parsed, output_file, indent=4)
 
 
 if __name__ == "__main__":
