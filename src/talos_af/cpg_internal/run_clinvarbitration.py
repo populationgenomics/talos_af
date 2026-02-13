2ffdb62c8ac337c38a549368e0a25d56f23287d5
MattWellie <mattwellie@gmail.com>
2026-02-13 11:43:46 +1000

    fix clinvarbitration generation

--- src/talos_af/cpg_internal/run_clinvarbitration.py
+++ src/talos_af/cpg_internal/run_clinvarbitration.py
@@ -24,28 +24,28 @@ def run_clinvarbitration_in_full(clinvar_file_tmp: Path, final_output: Path) -> 
     job.image(config.config_retrieve(['workflow', 'driver_image']))
     job.command('set -eo pipefail')
 
-    submission_name = 'submission_summary.txt.gz'
-    variant_name = 'variant_summary.txt.gz'
-
     # region: download clinvar source files
-    # for each input, if the file exists in GCP temp, use it, otherwise download a fresh one, write it to temp and GCP
-    for filename in [submission_name, variant_name]:
-        temp_gcp_file = clinvar_file_tmp / filename
-        if not temp_gcp_file.exists():
-            # download with wget, write to stdout, use tee to write that to GCP temp, and a local file. Nice.
-            job.command(
-                f'wget -q {CLINVAR_FTP}/{filename} -O {job[filename]}',
-            )
-            batch_instance.write_output(job[filename], temp_gcp_file)
-        else:
-            job[filename] = batch_instance.read_input(temp_gcp_file)
+    # this could be written more elegantly, but it's not worth thinking through
+    submission_name = 'submission_summary.txt.gz'
+    if (clinvar_file_tmp / submission_name).exists():
+        job.subs = batch_instance.read_input(clinvar_file_tmp / submission_name)
+    else:
+        job.command(f'wget -q {CLINVAR_FTP}/{submission_name} -O {job.subs}')
+        batch_instance.write_output(job.subs, clinvar_file_tmp / submission_name)
+
+    variant_name = 'variant_summary.txt.gz'
+    if (clinvar_file_tmp / variant_name).exists():
+        job.vars = batch_instance.read_input(clinvar_file_tmp / variant_name)
+    else:
+        job.command(f'wget -q {CLINVAR_FTP}/{variant_name} -O {job.vars}')
+        batch_instance.write_output(job.vars, clinvar_file_tmp / variant_name)
     # endregion
 
     # region: make new summary
     job.command(f"""
         python3 -m clinvarbitration.scripts.resummarise_clinvar \\
-            -v {job[variant_name]} \\
-            -s {job[submission_name]} \\
+            -v {job.vars} \\
+            -s {job.subs} \\
             -o $BATCH_TMPDIR/clinvarbitration \\
             --all_vcf $BATCH_TMPDIR/clinvarbitration.vcf.bgz
     """)
